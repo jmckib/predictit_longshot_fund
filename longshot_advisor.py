8168b7ea8ce271d5e7de3262762a7f1d92afe1d2
Jeremy Velev <jmckib2@gmail.com>
2022-02-28 09:24:30 -0800

    Print csv with two sections for soonest-expiring and most-profitable contracts.

--- longshot_advisor.py
+++ longshot_advisor.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+import csv
 import datetime
 from dateutil import parser
 import math
 from operator import itemgetter
 import requests
+import sys
 
 from scipy.stats import norm
 
@@ -41,12 +43,12 @@ def get_contract_data():
                     date_end = None
                 else:
                     try:
-                        date_end = parser.parse(date_end)
+                        date_end = parser.parse(date_end).date()
                     except parser.ParserError:
                         # There are a few dates that look like "02/25/2022
                         # 11:00:00 AM (ET)". We'll ignore the time portion.
                         date_end = datetime.datetime.strptime(
-                            date_end[:10], "%m/%d/%Y")
+                            date_end[:10], "%m/%d/%Y").date()
 
                 contracts.append({
                     'market_name': market["name"],
@@ -65,18 +67,26 @@ def get_contract_data():
 
 if __name__ == '__main__':
     data = get_contract_data()
-    data.sort(key=itemgetter('total_profit'), reverse=True)
+    fieldnames = ['market_name', 'contract_name', 'contract_key', 'price',
+                  'debiased_price', 'profit_per_share', 'total_profit',
+                  'total_profit_minus_fees', 'end_date', 'url']
 
-    print("Top 5 most profitable contracts:")
-    for contract in data[:5]:
-        print(f"\n{contract['market_name']}:\n{contract['url']}")
-        if contract['contract_name'] is None:
-            print(f"\t{contract['contract_key']}")
-        else:
-            print(f"\t{contract['contract_name']}: {contract['contract_key']}")
-        print(f"\tProfit: ${contract['total_profit']}, "
-              f"Minus fees: ${contract['total_profit_minus_fees']}, "
-              f"Price: ${contract['price']}, "
-              f"Debiased Price: ${contract['debiased_price']}")
-        if contract['end_date'] is not None:
-            print(f"\tEnd date: {contract['end_date']}")
+    print("Profitable contracts expiring within two weeks:")
+    two_weeks_from_now = (datetime.date.today() +
+                          datetime.timedelta(days=14))
+    with_dates = [
+        item for item in data
+        if item["end_date"] is not None
+        and item["end_date"] < two_weeks_from_now
+        and item["total_profit"] > 100
+    ]
+    writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(with_dates)
+
+    print("")
+    print("The most profitable 100 contracts with no end date:")
+    without_dates = [item for item in data if item["end_date"] is None]
+    without_dates.sort(key=itemgetter('total_profit'), reverse=True)
+    writer.writeheader()
+    writer.writerows(without_dates[:100])
